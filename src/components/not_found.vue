672cb020569d3e5b297ff38e05cb05eb32acf6e4
Shura-X <48450240+Shura-X@users.noreply.github.com>
2020-12-02 15:29:29 +0500

    added not_found component

--- src/components/not_found.vue
+++ src/components/not_found.vue
@@ -1,5 +1,8 @@
 <template>
-    <p>not found</p>
+    <main>
+        <p id="title">Oops...</p>
+        <p>This page doesn't exist</p>
+    </main>
 </template>
 
 <script>
@@ -7,3 +10,21 @@ export default {
     name: 'not_found'
 }
 </script>
+
+
+<style lang="sass" scoped>
+@import '../styles/main.sass'
+
+main 
+    font-size: 24px
+    font-family: 'Roboto'
+    @extend %centralize
+    flex-direction: column
+
+#title, p
+    margin: 0
+
+#title
+    margin-bottom: 1.5em
+    font-size: 1.2em
+</style>
